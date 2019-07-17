fc1c6d0b3d4d26df04c54a54174e95675c421cce
colso11 <colso110@gmail.com>
2019-07-17 15:40:22 +0900

    Update web_set_charset.sh

--- web_set_charset.sh
+++ web_set_charset.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-
-W_PID=$(docker ps | grep dc_twal_web_1 | awk '{print $1}')
-docker exec -it $W_PID echo "AddDefaultCharset utf-8" >> /usr/local/apache2/conf/httpd.conf
-docker exec -it $W_PID echo  "IndexOptions Charset=utf-8" >> /usr/local/apache2/conf/httpd.conf
-docker exec -it $W_PID echo "IndexOptions FancyIndexing VersionSort NameWidth=* HTMLTable +Charset=UTF-8"  >> /usr/local/apache2/conf/httpd.conf
+W_PID=$(docker ps | grep dc_twal_web | awk '{print $1}')
+docker exec -it $W_PID bash -c 'echo "AddDefaultCharset utf-8" >> /usr/local/apache2/conf/httpd.conf'
+docker exec -it $W_PID bash -c 'echo  "IndexOptions Charset=utf-8" >> /usr/local/apache2/conf/httpd.conf'
+docker exec -it $W_PID bash -c 'echo "IndexOptions FancyIndexing VersionSort NameWidth=* HTMLTable +Charset=UTF-8"  >> /usr/local/apache2/conf/httpd.conf'
 sleep 2
 docker-compose restart web
