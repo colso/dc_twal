41c2aa62fad330b4c7a013725a19d835c16592af
colso <colso110@gmail.com>
2019-07-18 17:58:42 +0900

    update db script

--- db_querys.sh
+++ db_querys.sh
@@ -2,10 +2,10 @@
 
 M_PID=$(docker ps | grep dc_twal_mariadb | awk '{print $1}')
 docker exec -it $M_PID mysql -uroot -proot mysql -e "CREATE DATABASE TWAL_DB default character set utf8mb4"
-docker exec -it $M_PID mysql -uroot -proot mysql -e " create table if not exists TWAL_DB.CANDI_TBL ( HASH VARCHAR(64) NULL, TITLE VARCHAR(128) NULL, ADD_TIME INT NULL, CATEGORY VARCHAR(16) NULL, SITENAME VARCHAR(64) NULL )"
+docker exec -it $M_PID mysql -uroot -proot mysql -e " create table if not exists TWAL_DB.CANDI_TBL ( HASH VARCHAR(64) NULL, TITLE VARCHAR(128) NULL, ADD_TIME INT NULL, STATUS INT NULL, CATEGORY VARCHAR(16) NULL, SITENAME VARCHAR(64) NULL )"
 docker exec -it $M_PID mysql -uroot -proot mysql -e "ALTER TABLE TWAL_DB.CANDI_TBL add constraint ret_hash primary key (hash)"
 docker exec -it $M_PID mysql -uroot -proot mysql -e "ALTER TABLE TWAL_DB.CANDI_TBL CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
-docker exec -it $M_PID mysql -uroot -proot mysql -e "create table if not exists TWAL_DB.RUN_TBL ( HASH VARCHAR(64) NULL, TITLE VARCHAR(128) NULL, ADD_TIME INT NULL, CATEGORY VARCHAR(16) NULL, SITENAME VARCHAR(64) NULL )"
+docker exec -it $M_PID mysql -uroot -proot mysql -e "create table if not exists TWAL_DB.RUN_TBL ( HASH VARCHAR(64) NULL, TITLE VARCHAR(128) NULL, ADD_TIME INT NULL, STATUS INT NULL, CATEGORY VARCHAR(16) NULL, SITENAME VARCHAR(64) NULL )"
 docker exec -it $M_PID mysql -uroot -proot mysql -e "ALTER TABLE TWAL_DB.RUN_TBL add constraint ret_hash primary key (hash)"
 docker exec -it $M_PID mysql -uroot -proot mysql -e "ALTER TABLE TWAL_DB.RUN_TBL CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
 docker exec -it $M_PID mysql -uroot -proot mysql -e "create table if not exists TWAL_DB.RET_TBL ( HASH VARCHAR(64) NULL, TITLE VARCHAR(128) NULL, RET_TIME INT NULL, CATEGORY VARCHAR(16) NULL, SITENAME VARCHAR(64) NULL )"
